5951b39b578664c6f8211a7fc0a73b26e8f45e1d
Сергей Винчестер <creepmah@gmail.com>
2025-02-12 10:26:09 +0300

    A configuration project has been added.

---
+++ KoshelekRu_TestTask.Domain/Models/AppSettings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Domain.Models.AppSettings
+{
+    public class AppSettings
+    {
+        public SignalRSettings? SignalRSettings { get; set; }
+        public DBConfig? DBConfig { get; set; }
+    }
+}
